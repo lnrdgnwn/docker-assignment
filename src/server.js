ab2ac411d81a70a80644c5ae2cfd4a156849557c
lnrdgnwn <leonardogunawan15@gmail.com>
2026-03-28 16:05:42 +0700

    feat(server): intialize express server and configurating route

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+
+app.use(express.json());
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.APP_PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
